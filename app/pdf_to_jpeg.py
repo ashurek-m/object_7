e3a7ee5e71b94b0fe9f949aa8cc147b9e16a2ace
oshurek_m <thedarthset@gmail.com>
2022-09-05 15:53:44 +0300

    up fun "convert"

--- app/pdf_to_jpeg.py
+++ app/pdf_to_jpeg.py
@@ -5,17 +5,16 @@ import glob
 import PIL
 
 PIL.Image.MAX_IMAGE_PIXELS = None
-outputDir = 'output/'
 
 
-def convert(file, outputDir):
+def convert(name_file, outputDir):
     if not os.path.exists(outputDir):
         os.makedirs(outputDir)
 
     pages = convert_from_path(file, 250)
     counter = 1
     for page in pages:
-        myfile = outputDir / (file.split('\\')[-1][0:-4] + f'_{str(counter)}' + '.jpg')
+        myfile = outputDir / (name_file.split('\\')[-1][0:-4] + f'_{str(counter)}' + '.jpg')
         counter += 1
         page.save(myfile, 'JPEG')
 
@@ -27,4 +26,3 @@ file_list = glob.glob(f'{str(input_dir)}\\*.pdf')
 
 for file in file_list:
     convert(file, output_dir)
-
