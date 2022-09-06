1da21b12e5276d48527e42757bccda192f32a813
oshurek_m <thedarthset@gmail.com>
2022-09-06 17:09:39 +0300

    создан касстомный клас для преобразования в датасет

--- app/my_object.py
+++ app/my_object.py
@@ -2,24 +2,17 @@ import os
 import pandas as pd
 from torch.utils.data import Dataset
 from torchvision.io import read_image
+from typing import List
 
 
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
+    def __init__(self, img_dir: List, transform=None):
         self.img_dir = img_dir
         self.transform = transform
-        self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.img_dir)
 
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
+    def __getitem__(self, item):
         if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
+            return self.transform(self.img_dir[item])
